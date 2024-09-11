d133d5cbe217e62ba090737f99bdfbdaa48d9d07
дмитрий Madara <madara@deeplay.io>
2024-09-11 11:24:19 +0600

    Edit timings

--- mmpy_bot/webhook_server.py
+++ mmpy_bot/webhook_server.py
@@ -82,7 +82,7 @@ class WebHookServer:
                     pass
             except Empty:
                 pass
-            await asyncio.sleep(.5)
+            await asyncio.sleep(0.0001)
 
     @handle_json_error
     async def process_webhook(self, request: web.Request):
